39d4a824caf304539aad4f48537626f8f8f01728
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-06-19 11:55:11 -0300

    Configured project index file.
    Got the routing, material theme and service worker configured.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import store, {history} from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const target = document.querySelector('#root');
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>,
+  target
+);
+
 registerServiceWorker();
